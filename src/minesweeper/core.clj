0f57ea6bae2ff1c6864969020a7b0040898fed84
Steinar Sturlaugsson <costeinar@gmail.com>
2017-09-16 01:21:13 +0200

    r

--- src/minesweeper/core.clj
+++ src/minesweeper/core.clj
@@ -10,7 +10,8 @@
 ; Example board, vectors of vectors containing field maps (see create-field)
 
 (defn create-field
-  "Creates a single field"
+  "Creates a single field. Surrounding count is initialized to zero and is set to a value higher than that
+   only if the user steps on a field near it"
   []
   {:bomb false :surrounding 0})
 
@@ -23,49 +24,49 @@
   [rows cols]
   {:fields (into [] (map (fn [ix] (create-field)) (range (* rows cols))))
    :rows rows
-   :cols cols})
-(create-board 3 3)
-
-(def dbg true)
+   :cols cols
+   :dead false})
 
 (defn field-to-string
   "Shows the field with all info for debug purposes"
-  [field]
+  [field dbg]
   (if dbg
-    (format "%s%d" (if (:bomb field) "b" "-") 0)
-    "x"))
+    (format "%s%d%s" (if (:bomb field) "b" "-") (:surrounding field) (if (:stepped field) "s" " "))
+    (let [stepped (:stepped field)
+          bomb (:bomb field)
+          surr (:surrounding field)]
+      (if stepped
+        (cond bomb "b"
+              (> surr 0) (str surr)
+              (= surr 0) "#")
+        "x"))))
 
 (defn row-to-string
-  [row]
-  (clojure.string/join "|" (map field-to-string row)))
+  [row dbg]
+  (clojure.string/join "|" (map #(field-to-string % dbg) row)))
 
 (defn board-to-string
-  [board]
+  [board dbg]
   (let [fields (:fields board)
         rows (:rows board)
         cols (:cols board)
         row-lists (partition cols fields)
         row-lists-str (map row-to-string row-lists)]
-    (clojure.string/join "\n" (map row-to-string row-lists))))
-(board-to-string (create-board 2 3))
+    (clojure.string/join "\n" (map #(row-to-string % dbg) row-lists))))
 
-(assert (= (board-to-string (create-board 2 3))
-           "-0|-0|-0\n-0|-0|-0"))
-
-(defn ix-to-coord
+(defn index-to-coord
   "Maps from a index to coordinate"
-  [ix rows cols]
-  (let [row (int (/ ix cols))
+  [ix board]
+  (let [rows (:rows board)
+        cols (:cols board)
+        row (int (/ ix cols))
         col (rem ix cols)]
     [row col]))
-(map #(ix-to-coord % 3 2) (range (* 3 2))) ; i
-
 
 (defn random-bombs
   "Returns count number of bomb locations in an row x col board"
   [bombs fields]
   (take bombs (shuffle (range fields))))
-(random-bombs 3 9)
 
 (defn place-bombs
   "Given a board, places bombs randomly and returns a new board with those placed"
@@ -77,15 +78,131 @@
             board
             bombs)))
 
+(defn parse-input
+  [line]
+  (map read-string (clojure.string/split line #",")))
+(parse-input "0,2")
+
+(defn get-input
+  "Waits for user to enter text and hit enter, then cleans the input"
+  ([] (get-input nil))
+  ([default]
+   (let [input (clojure.string/trim (read-line))]
+     (if (empty? input)
+       default
+       (parse-input input)))))
+
+(defn make-move
+  [board ix]
+  ; TODO: Idiomatic way of setting two values on the same object, where one is conditional 
+  (let [board (if (get-in board [:fields ix :bomb])
+                (assoc board :dead true) board)
+        board (assoc-in board [:fields ix :stepped] true)]
+    board))
+
+(defn add-coords
+  [a b]
+  [(+ (get a 0) (get b 0))
+   (+ (get a 1) (get b 1))])
+(add-coords [1 1] [0 0])
+
+(defn get-surrounding-square
+  "Returns the square surrounding this coordinate. Does not filter illegal coordinates"
+  [coord]
+  (let [deltas (for [x [-1 0 1] y [-1 0 1]] (vector x y))]
+    (map #(add-coords % coord) deltas)))
+(get-surrounding-square [0 0])
+
+(defn is-out-of-bounds?
+  [coord board]
+  (let [[row col] coord
+        rows (:rows board)
+        cols (:cols board)]
+    (or (< row 0) (< col 0)
+        (>= row rows) (>= col cols))))
+(is-out-of-bounds? [3 2] (create-board 3 3))
+
+(defn coord-to-index
+  [board coord]
+  (let [[row col] coord
+        rows (:rows board)
+        cols (:cols board)]
+    (+ (* row cols) col)))
+(coord-to-index (create-board 3 3) [0 0])
+(coord-to-index (create-board 3 3) [0 1])
+(coord-to-index (create-board 3 3) [2 0])
+
+(defn get-surrounding
+  "Returns all fields surrounding this square, excluding those that are out of bounds"
+  [board ix]
+  (let [rows (:rows board)
+        cols (:cols board)
+        coord (index-to-coord ix board)]
+
+    (map #(coord-to-index board %) (remove #(is-out-of-bounds? % board)
+                                           (get-surrounding-square coord)))))
+(get-surrounding (create-board 9 9) 12)
+
+(-> (create-board 3 3)
+    (#(place-bombs % [0]))
+    (#(make-move % 0))
+    (#(get-surrounding % 1)))
+
+(defn game-over?
+  [board]
+  (:exploded board))
+
+(defn game-over
+  [board]
+  (println "sorry, game over :(")
+  (println (board-to-string board)))
+
+(declare user-entered-valid-move user-entered-invalid-move)
+
+(defn prompt-move
+  "Prompts the user for a move. The move is fetched via the input-fn, which should return a board coordinate"
+  [input-fn board]
+  (println "Current state:")
+  (println (board-to-string board))
+  (println "Select a field, e.g. 0,0:")
+  (let [input (input-fn)]
+    (if-let [new-board (make-move board input)]
+      (user-entered-valid-move new-board)  ; todo stack?
+      (user-entered-invalid-move board))))
+
+(defn user-entered-valid-move
+  [board]
+  (if (game-over? board)
+    (game-over board)
+    (prompt-move board)))
+
+(defn user-entered-invalid-move
+  [board]
+  (println "\n!!! That was an invalid move :(\n")
+  (prompt-move board))
+
+(defn all-moves
+  [rows cols]
+  (map #(index-to-coord % rows cols) (range (* rows cols))))
+(all-moves 3 3)
+
 (defn game
-  []
-  (let [rows 9
-        cols 9
-        bombs 10
+  [prompt-move]
+  (let [rows 3
+        cols 3
+        bombs 3
         fields (* rows cols)
         board (create-board rows cols)
-        board-with-bombs (place-bombs board (random-bombs bombs fields))]
-    (println (board-to-string board-with-bombs))))
+        board (place-bombs board (random-bombs bombs fields))]
+    (prompt-move board)))
+  ; (let [rows 3
+  ;       cols 3
+  ;       bombs 3
+  ;       fields (* rows cols)
+  ;       board (create-board rows cols)
+  ;       board (place-bombs board (random-bombs bombs fields))]
+  ;   (prompt-move board (fn [] [0, 1]))))
+;(game (partial prompt-move (fn [] [0, 1])))
 
 (defn -main
   "Runs a game of minesweeper, that's all"
--- test/minesweeper/core_test.clj
+++ test/minesweeper/core_test.clj
@@ -2,11 +2,51 @@
   (:require [clojure.test :refer :all]
             [minesweeper.core :refer :all]))
 
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 1 1))))
+(deftest test-board-to-string
+  (testing "A newly initialized board is shown correctly"
+    (let [board (create-board 2 3)]
+      (is (= (board-to-string board true)
+             "-0 |-0 |-0 \n-0 |-0 |-0 "))
+      (is (= (board-to-string board false)
+             "x|x|x\nx|x|x")))))
 
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (place-bombs (create-board 3 3) 3 3 3))))
-    
+(deftest test-board-with-bombs-to-string
+  (testing "A board with bombs is shown correctly"
+    (let [board (place-bombs
+                 (create-board 2 3) [0 2 5])]
+      (is (= (board-to-string board true)
+             "b0 |-0 |b0 \n-0 |-0 |b0 "))
+      (is (= (board-to-string board false)
+             "x|x|x\nx|x|x")))))
+
+; In the following tests, we have the following board:
+; b0 |-0 |-0 
+; -0 |-0 |-0  
+; If the user steps on [0,0], he dies
+; If the user steps on any of those surrounding [0,0], that field should change to '-1s'
+; where s stands for "stepped on". Other fields should not change.
+; If the user steps on [0,2] or [1,2], they should change to '-0s' and they should recursively
+; affect surrounding squares, until a square that has a bomb next to it is reached.
+(deftest test-stepped-on-dies
+  (testing "A stepped on board with bombs is shown correctly"
+    (let [board (create-board 2 3)
+          board (place-bombs board [0])
+          board (make-move board 0)]
+      (is (= (board-to-string board true)
+             "b0s|-0 |-0 \n-0 |-0 |-0 "))
+      (is (= (board-to-string board false)
+             "b|x|x\nx|x|x"))
+      (is (:dead board)))))
+
+; Selecting [1,2] in this case should lead to: 
+; x|1|#
+; x|1|#
+(deftest test-stepped-on-spreads
+  (testing "A stepped on board with bombs is shown correctly"
+    (let [board (create-board 2 3)
+          board (place-bombs board [0])
+          board (make-move board 5)]
+      (is (= (board-to-string board true)
+             "b0 |-0 |-0 \n-0 |-0 |-0s"))
+      (is (= (board-to-string board false)
+             "x|1|#\nx|1|#")))))
